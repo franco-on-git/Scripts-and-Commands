a957069f3f77b028fe473666f634df7b75878779
Francisco Perez <fpere47@safeway.com>
2025-04-30 13:34:52 -0700

    Edited Query-Storage-Disk-Utilization.ps1 script

--- QUERY/Query-Storage-Disk-Utilization.ps1
+++ QUERY/Query-Storage-Disk-Utilization.ps1
@@ -14,15 +14,14 @@
 
 # -------------------------------------------------------------------------------------------------------------
 # QUERY LOCALLY ATTACHED DISKS ONLY (TYPE 3)
-Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3} | 
-                                         Select-Object @{E={$_.__Server};L="Hostname"},
-                                         @{L="Drive";E={$_.DeviceID}}, 
-                                         @{L="Capacity(GB)";E={[math]::round($_.Size/1GB,2)}}, 
-                                         @{L="Used(GB)";E={[math]::round($_.Size/1GB - $_.FreeSpace/1GB,2)}},                                          
-                                         @{L="(%)Used";E={[Math]::Round(((($_.Size - $_.FreeSpace) / $_.Size) * 100),2)}},
-                                         @{L="Free(GB)";E={[math]::round($_.FreeSpace/1GB,2)}}, 
-                                         @{L="(%)Free";E={[Math]::round((($_.freespace/$_.size) * 100),2)}} | 
-                                         Out-GridView
+Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3} | Select-Object @{L="Hostname";E={$_.__Server}},
+                                                                                           @{L="Drive";E={$_.DeviceID}}, 
+                                                                                           @{L="Capacity(GB)";E={[math]::round($_.Size/1GB,2)}}, 
+                                                                                           @{L="Used(GB)";E={[math]::round($_.Size/1GB - $_.FreeSpace/1GB,2)}},                                          
+                                                                                           @{L="(%)Used";E={[Math]::Round(((($_.Size - $_.FreeSpace) / $_.Size) * 100),2)}},
+                                                                                           @{L="Free(GB)";E={[math]::round($_.FreeSpace/1GB,2)}}, 
+                                                                                           @{L="(%)Free";E={[Math]::round((($_.freespace/$_.size) * 100),2)}} | 
+                                                                             Out-GridView
 
 
 
@@ -34,15 +33,15 @@ $DiskType = @{
     4 = "NETWORK"
     5 = "CD-ROM"}
  
- Get-WmiObject -Class Win32_LogicalDisk  | Select-Object @{E={$_.__Server};L="Hostname"},
-                                          @{L="Drive";E={$_.DeviceID}}, 
-                                          @{L='Type';E={$DiskType.item([int]$_.DriveType)}},
-                                          @{L="Capacity(GB)"; E={[math]::round($_.Size/1GB,2)}}, 
-                                          @{L="Used(GB)"; E={[math]::round($_.Size/1GB - $_.FreeSpace/1GB,2)}},                                          
-                                          @{L="(%)Used";E= {[Math]::Round(((($_.Size - $_.FreeSpace) / $_.Size) * 100),2)}},
-                                          @{L="Free(GB)"; E={[math]::round($_.FreeSpace/1GB,2)}}, 
-                                          @{L="(%)Free";E={[Math]::round((($_.freespace/$_.size) * 100),2)}} | 
-                                          Out-GridView
+ Get-WmiObject -Class Win32_LogicalDisk  | Select-Object @{L="Hostname";E={$_.__Server}},
+                                                         @{L="Drive";E={$_.DeviceID}}, 
+                                                         @{L='Type';E={$DiskType.item([int]$_.DriveType)}},
+                                                         @{L="Capacity(GB)"; E={[math]::round($_.Size/1GB,2)}}, 
+                                                         @{L="Used(GB)"; E={[math]::round($_.Size/1GB - $_.FreeSpace/1GB,2)}},                                          
+                                                         @{L="(%)Used";E= {[Math]::Round(((($_.Size - $_.FreeSpace) / $_.Size) * 100),2)}},
+                                                         @{L="Free(GB)"; E={[math]::round($_.FreeSpace/1GB,2)}}, 
+                                                         @{L="(%)Free";E={[Math]::round((($_.freespace/$_.size) * 100),2)}} | 
+                                           Out-GridView
                                         
 
 
