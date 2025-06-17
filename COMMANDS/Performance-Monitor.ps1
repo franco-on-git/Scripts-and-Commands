8399b0771bad6d0a6c269bf7fc46f48c8618753f
Francisco Perez <fpere47@safeway.com>
2025-06-17 11:16:02 -0700

    Modified Perormance-Monitor script to match logman names

--- COMMANDS/Performance-Monitor.ps1
+++ COMMANDS/Performance-Monitor.ps1
@@ -3,11 +3,11 @@
 # 5 Second Interval sampling
 # 1GB max file size
 # !!! Run as Administrator in CMD !!!
-Logman.exe create counter _LogMan_5SecInterval -f bincirc -v mmddhhmm -max 1000 -c "\LogicalDisk(*)\*" "\Memory\*" "\Cache\*" "\Network Interface(*)\*" "\Paging File(*)\*" "\PhysicalDisk(*)\*" "\Processor(*)\*" "\Processor Information(*)\*" "\Process(*)\*" "\Redirector\*" "\Server\*" "\System\*" "\Server Work Queues(*)\*" "\Terminal Services\*" -si 00:00:05 
+Logman.exe create counter _LOGMAN_ALL_5SEC -f bincirc -v mmddhhmm -max 1000 -c "\LogicalDisk(*)\*" "\Memory\*" "\Cache\*" "\Network Interface(*)\*" "\Paging File(*)\*" "\PhysicalDisk(*)\*" "\Processor(*)\*" "\Processor Information(*)\*" "\Process(*)\*" "\Redirector\*" "\Server\*" "\System\*" "\Server Work Queues(*)\*" "\Terminal Services\*" -si 00:00:05 
 
 # Start\Stop Data Collector: 
-logman start _LOG_ALL_5SEC
-logman stop  _LOG_ALL_5SEC
+logman start _LOGMAN_ALL_5SEC
+logman stop  _LOGMAN_ALL_5SEC
 
 
 # COLLECT PROCESS/CPU ONLY ----------------------------------------------------------------------------------
