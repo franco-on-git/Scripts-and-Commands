22b1dbdb70bbaa44b4207573ebedc944354156da
Franco <quincy.chavez@outlook.com>
2025-07-12 01:33:36 -0700

    udpates descritpions on scripts

--- SCRIPTS/QUERY/Query-Printers.ps1
+++ SCRIPTS/QUERY/Query-Printers.ps1
@@ -1,5 +1,29 @@
-# If Printer EXISTS
-$Printers = get-printer | Where-Object {$_.name -notlike "*xps*" -and $_.Name -notlike "*pdf*"}
+<#
+    .NOTES
+        Author: Franco-hq
+        Created: 09/2024
 
-if ($null -ne $Printers) {Write-Host "Printer Mappings Found"}
-Else {Write-Host "No Printers"}
+    .SYNOPSIS
+        Query local system for installed printers.
+
+    .DESCRIPTION
+        1. Creates a variable that queries for all locally installed printers excluding "XPS" and "PDF".
+        2. Checks if any printers are installed or if the result is $null, and shows an appropriate message.
+#>
+
+
+# Save all printers to a variable, excluding XPS and PDF ones.
+$Printers = Get-Printer | Where-Object {
+    $_.Name -notlike "*xps*" -and $_.Name -notlike "*pdf*"
+}
+
+# Check if any printers were found and display appropriate result
+if ($null -ne $Printers) {
+    Write-Host "Printer Mappings Found:"
+
+    # Display printer info in a readable table format
+    $Printers | Format-Table Name, DriverName, PortName -AutoSize
+}
+else {
+    Write-Host "No Printers found"
+}
