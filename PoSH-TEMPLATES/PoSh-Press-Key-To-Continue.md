6b35ab6754bec73cd643553912d0c34c16ddd0b6
Franco <104126259+franco-hq@users.noreply.github.com>
2025-09-21 14:04:33 -0700

    Update and rename PoSh-Press-Key-To-Continue.ps1 to PoSh-Press-Key-To-Continue.md

---
+++ PoSH-TEMPLATES/PoSh-Press-Key-To-Continue.md
@@ -0,0 +1,11 @@
+> [!NOTE]
+> - Displays a message at the terminal
+> - Paste "PressAnyKeyToContinue" at the end when exiting script 
+> - Copy Function below to Top of Script
+
+# Copy Command:
+```
+Function PressAnyKeyToContinue {
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');}
+```
--- PoSH-TEMPLATES/PoSh-Press-Key-To-Continue.ps1
+++
@@ -1,8 +0,0 @@
-# Displays a message at the terminal
-# Paste "PressAnyKeyToContinue" at the end when exiting script 
-# Copy Function below to Top of Script
-
-# ----------------------------------------------------------------------------------
-Function PressAnyKeyToContinue {
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');}
