7fc0a6023a7de54959cdc9b45f7b76009afcb323
Franco <104126259+franco-hq@users.noreply.github.com>
2025-09-21 13:58:17 -0700

    Update PoSh-Label-Name-Change.ps1

--- PoSH-TEMPLATES/PoSh-Label-Name-Change.ps1
+++ PoSH-TEMPLATES/PoSh-Label-Name-Change.ps1
@@ -1,3 +1,6 @@
-# Change the name of a property in results
+> [!NOTE]
+> Change the name of a property in results
+
+# PowerShell
 @{Expression={$_.samaccountname};Label="GroupMemebership"}
 @{E={$_.samaccountname};L="GroupMemebership"} #Short form
